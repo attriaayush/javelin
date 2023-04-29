0cd8d5620ac095fb33d03fb75e372adc2531dfb5
Aayush Attri <aayush.attri@snyk.io>
2023-04-29 14:50:17 +0100

    upload file to s3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,143 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::Path;
+
+use clap::Parser;
+use aws_sdk_s3::error::SdkError;
+use aws_sdk_s3::operation::create_multipart_upload::CreateMultipartUploadOutput;
+use aws_sdk_s3::operation::get_object::{GetObjectOutput, GetObjectError};
+use aws_smithy_http::byte_stream::{ByteStream, Length};
+use aws_sdk_s3::Client;
+use aws_sdk_s3::types::{CompletedPart, CompletedMultipartUpload};
+
+//In bytes, minimum chunk size of 5MB. Increase CHUNK_SIZE to send larger chunks.
+const CHUNK_SIZE: u64 = 1024 * 1024 * 5;
+const MAX_CHUNKS: u64 = 10000;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    bucket_name: String,
+
+    #[arg(short, long)]
+    file_path: String,
+}
+
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+
+    upload_multipart(&args.file_path, &args.bucket_name).await.unwrap();
+}
+
+async fn upload_multipart(file_path: &str, bucket_name: &str) -> anyhow::Result<()> {
+    let shared_config = aws_config::load_from_env().await;
+    let client = Client::new(&shared_config);
+
+
+    let path = Path::new(file_path);
+    let file_size = tokio::fs::metadata(path)
+        .await
+        .expect("it exists I swear")
+        .len();
+
+    let multipart_upload_res: CreateMultipartUploadOutput = client
+        .create_multipart_upload()
+        .bucket(bucket_name)
+        .key(file_path)
+        .send()
+        .await
+        .unwrap();
+
+    let upload_id = multipart_upload_res.upload_id().unwrap();
+
+    let mut chunk_count = (file_size / CHUNK_SIZE) + 1;
+    let mut size_of_last_chunk = file_size % CHUNK_SIZE;
+    if size_of_last_chunk == 0 {
+        size_of_last_chunk = CHUNK_SIZE;
+        chunk_count -= 1;
+    }
+
+    if file_size == 0 {
+        panic!("Bad file size.");
+    }
+    if chunk_count > MAX_CHUNKS {
+        panic!("Too many chunks! Try increasing your chunk size.")
+    }
+
+    let mut upload_parts: Vec<CompletedPart> = Vec::new();
+
+    for chunk_index in 0..chunk_count {
+        let this_chunk = if chunk_count - 1 == chunk_index {
+            size_of_last_chunk
+        } else {
+            CHUNK_SIZE
+        };
+
+        let stream = ByteStream::read_from()
+            .path(path)
+            .offset(chunk_index * CHUNK_SIZE)
+            .length(Length::Exact(this_chunk))
+            .build()
+            .await
+            .unwrap();
+        
+        //Chunk index needs to start at 0, but part numbers start at 1.
+        let part_number = (chunk_index as i32) + 1;
+
+        // snippet-start:[rust.example_code.s3.upload_part]
+        let upload_part_res = client
+            .upload_part()
+            .key(file_path)
+            .bucket(bucket_name)
+            .upload_id(upload_id)
+            .body(stream)
+            .part_number(part_number)
+            .send()
+            .await?;
+
+        upload_parts.push(
+            CompletedPart::builder()
+                .e_tag(upload_part_res.e_tag.unwrap_or_default())
+                .part_number(part_number)
+                .build(),
+        );
+    };
+
+    let completed_multipart_upload: CompletedMultipartUpload = CompletedMultipartUpload::builder()
+        .set_parts(Some(upload_parts))
+        .build();
+
+    let _complete_multipart_upload_res = client
+        .complete_multipart_upload()
+        .bucket(bucket_name)
+        .key(file_path)
+        .multipart_upload(completed_multipart_upload)
+        .upload_id(upload_id)
+        .send()
+        .await
+        .unwrap();
+
+    let data: GetObjectOutput = download_object(&client, &bucket_name, file_path).await?;
+    let data_length: u64 = data.content_length().try_into().unwrap();
+    if file_size == data_length {
+        println!("Data lengths match.");
+    } else {
+        println!("The data was not the same size!");
+    }
+    Ok(())
+}
+
+async fn download_object(
+    client: &Client,
+    bucket_name: &str,
+    key: &str,
+) -> Result<GetObjectOutput, SdkError<GetObjectError>> {
+    client
+        .get_object()
+        .bucket(bucket_name)
+        .key(key)
+        .send()
+        .await
 }
